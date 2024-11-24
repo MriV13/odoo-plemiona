48229d3e3c142bb1468b24698d551a1a7c011210
mariusztt <mariuszpodzo@o2.pl>
2024-11-24 13:48:19 +0100

    Add initial module for plemiona

---
+++ website_plemiona/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Website - Plemiona',
+    'version': '17.0.1.0.0',
+    'depends': ['website'],
+    'summary': ".",
+    'description': """.""",
+    'demo': [
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+}
